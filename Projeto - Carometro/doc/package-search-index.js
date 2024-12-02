cfa5afdf30430156f7f4fdf16f09b583d0a74597
Ricardo Rosendo <124933384+rickicr-collab@users.noreply.github.com>
2024-12-02 07:55:49 -0300

    Delete Projeto - Carometro directory

--- Projeto - Carometro/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"model"},{"l":"utils"},{"l":"view"}];updateSearchResults();
--- Projeto - Carometro/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"view","l":"Carometro"},{"p":"model","l":"DAO"},{"p":"view","l":"Sobre"},{"p":"utils","l":"Validador"}];updateSearchResults();
